9d92769d0327506371de72054e6f705b30943c39
Alessio Vertemati <alessio.vertemati@gmail.com>
2022-12-29 12:03:14 +0100

    wip

--- app/Http/Controllers/ChampionshipController.php
+++ app/Http/Controllers/ChampionshipController.php
@@ -6,6 +6,8 @@ use App\Models\Championship;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class ChampionshipController extends Controller
 {
@@ -93,7 +95,7 @@ class ChampionshipController extends Controller
      */
     public function edit(Championship $championship)
     {
-        //
+        return view('championship.edit', ['championship' => $championship]);
     }
 
     /**
@@ -105,7 +107,34 @@ class ChampionshipController extends Controller
      */
     public function update(Request $request, Championship $championship)
     {
-        //
+        // update is permitted unless races are defined
+        // if races are there no changes can be made
+
+        if($championship->races()->exists()){
+            throw ValidationException::withMessages(['races' => __('Championship contains races. Update is allowed only when no races are present.')]);
+        }
+
+        $validated = $this->validate($request, [
+            'start' => 'required|date|after:yesterday',
+            'end' => 'nullable|date|after:start',
+            'title' => ['nullable', 'string', 'max:250', Rule::unique((new Championship())->getTable(), 'title')->ignore($championship)],
+            'description' => 'nullable|string|max:1000',
+        ]);
+
+        $start_date = Carbon::parse($validated['start']);
+        $end_date = $validated['end'] ? Carbon::parse($validated['end']) : null;
+
+        $championship->update([
+            'title' => $validated['title'],
+            'description' => $validated['description'],
+            'start_at' => $start_date,
+            'end_at' => $end_date,
+        ]);
+
+        return to_route('championships.show', $championship)
+            ->with('message', __(':championship updated.', [
+                'championship' => $validated['title']
+            ]));
     }
 
     /**
--- tests/Feature/ChampionshipTest.php
+++ tests/Feature/ChampionshipTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Championship;
+use App\Models\Race;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
@@ -121,5 +122,67 @@ class ChampionshipTest extends TestCase
             ]);
 
         $response->assertForbidden();
+    }
+
+    public function test_championship_can_be_updated()
+    {
+        $user = User::factory()->organizer()->create();
+
+        $existing = Championship::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->from(route('championships.show', $existing))
+            ->put(route('championships.update', $existing), [
+                'start' => '2023-03-02',
+                'end' => '2023-12-02',
+                'title' => 'Kartsport changed 2023',
+                'description' => 'a little description',
+            ]);
+
+        $response->assertRedirectToRoute('championships.show', $existing);
+        
+        $response->assertSessionHasNoErrors();
+
+        $response->assertSessionHas('message', 'Kartsport changed 2023 updated.');
+
+        $champ = Championship::first();
+
+        $this->assertInstanceOf(Championship::class, $champ);
+        $this->assertEquals('Kartsport changed 2023', $champ->title);
+        $this->assertEquals('a little description', $champ->description);
+        $this->assertEquals(Carbon::parse('2023-03-02'), $champ->start_at);
+        $this->assertEquals(Carbon::parse('2023-12-02'), $champ->end_at);
+    }
+
+    public function test_championship_cannot_be_updated_when_races_are_present()
+    {
+        $user = User::factory()->organizer()->create();
+
+        $existing = Championship::factory()
+            ->has(Race::factory()->count(1))
+            ->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->from(route('championships.edit', $existing))
+            ->put(route('championships.update', $existing), [
+                'start' => '2023-03-02',
+                'end' => '2023-12-02',
+                'title' => 'Kartsport 2023',
+                'description' => 'a little description',
+            ]);
+
+        $response->assertRedirectToRoute('championships.edit', $existing);
+        
+        $response->assertSessionHasErrors('races', __('Championship contains races. Update is allowed only when no races are present.'));
+
+        $champ = Championship::first();
+
+        $this->assertInstanceOf(Championship::class, $champ);
+        $this->assertEquals($existing->title, $champ->title);
+        $this->assertEquals($existing->description, $champ->description);
+        $this->assertEquals($existing->start_at, $champ->start_at);
+        $this->assertEquals($existing->end_at, $champ->end_at);
     }
 }

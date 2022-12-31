6c62f8afe34a4fa3b4297b740d6fe39eb9ccf4b6
Alessio Vertemati <alessio.vertemati@gmail.com>
2022-12-31 10:43:48 +0100

    Add column to store pre-calculated registration opening

--- database/migrations/2022_12_25_173248_create_races_table.php
+++ database/migrations/2022_12_25_173248_create_races_table.php
@@ -21,6 +21,9 @@ return new class extends Migration
             $table->timestamps();
             $table->timestamp('event_start_at')->nullable();
             $table->timestamp('event_end_at')->nullable(); // in case it starts on saturday and end on sunday
+            
+            $table->timestamp('registration_opens_at')->nullable();
+            $table->timestamp('registration_closes_at')->nullable();
 
             $table->foreignIdFor(Championship::class);
 

5fb6c79b0f7e0f6564613cee4d0ca9da0b2927d7
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-03-21 21:00:42 +0100

    Sync italian translation

--- resources/views/livewire/race-number.blade.php
+++ resources/views/livewire/race-number.blade.php
@@ -2,7 +2,7 @@
     <x-jet-label for="bib" value="{{ __('Number') }}*" />
     <x-jet-input-error for="bib" class="mt-2" />
     <x-jet-input id="bib" type="number" name="bib" class="mt-1 block w-full" :value="$value"  autofocus />
-    <p class="text-zinc-600 mt-1">{{ __('Need a suggestion? try') }} 
+    <p class="text-zinc-600 mt-1">{{ __('Need a suggestion? Try') }} 
         @forelse ($suggestions as $item)
             <button 
                 type="button"

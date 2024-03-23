15805aa5887635a68fe752361ffcd330a13283c9
Alessio <avvertix@users.noreply.github.com>
2024-03-23 15:17:08 +0100

    improve mobile layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               
             <div>
                 <p class="uppercase tracking-widest text-sm mb-2">{{ __('Register to a race') }}</p>
             
-                <div class="grid grid-cols-3 gap-4">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             
                     @forelse ($races as $race)
                         <article class="p-4 shadow-lg bg-white rounded-md ring-2 ring-orange-300 shadow-orange-200">

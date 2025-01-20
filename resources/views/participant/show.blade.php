81e47a17cc7cbb3355912e1824f3a8ba13745da4
Alessio <avvertix@users.noreply.github.com>
2025-01-20 19:27:19 +0100

    Remove bonus checkbox as not active

--- resources/views/participant/show.blade.php
+++ resources/views/participant/show.blade.php
@@ -307,30 +307,6 @@
                 </div>
             </div>
 
-            <x-section-border />
-
-            <div class="md:grid md:grid-cols-3 md:gap-6">
-                <x-section-title>
-                    <x-slot name="title">{{ __('Bonus') }}</x-slot>
-                    <x-slot name="description">{{ __('The participant can use a bonus given by the organizer.') }}</x-slot>
-                </x-section-title>
-
-                <div class="mt-5 md:mt-0 md:col-span-2">
-
-                        <div class="px-4 py-5">
-                            <div class="grid grid-cols-6 gap-6">
-                                <div class="col-span-6 sm:col-span-4">
-                                    <label for="bonus" class="flex items-center">
-                                        <x-checkbox id="bonus" name="bonus" />
-                                        <span class="ml-2">{{ __('Apply a bonus') }}</span>
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </div>
-
-
         </div>
     </div>
 </x-app-layout>

a74ce9deff9bc052cb65d9ac77d2573003e837f8
avvertix <5672748+avvertix@users.noreply.github.com>
2025-01-20 08:48:51 +0000

    Fix styling

--- tests/Feature/Communications/CommunicationMessageTest.php
+++ tests/Feature/Communications/CommunicationMessageTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Communications;
 
-use App\Models\Communication;
 use App\Models\CommunicationMessage;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;

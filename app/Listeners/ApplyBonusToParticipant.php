8541575f2aeb5e4dc0f05e1f83de09c261a1b119
Alessio <avvertix@users.noreply.github.com>
2025-03-19 09:44:35 +0100

    Configuration to use one bonus at time and skip applying bonus if already applied

--- app/Listeners/ApplyBonusToParticipant.php
+++ app/Listeners/ApplyBonusToParticipant.php
@@ -23,11 +23,16 @@ class ApplyBonusToParticipant
     {
         $championship = $event->race->championship;
 
-        $fixedBonusAmount = $championship->bonuses->fixed_bonus_amount ?? (int) config('races.bonus_amount'); // TODO: read from config if not set
+        $fixedBonusAmount = $championship->bonuses->fixed_bonus_amount ?? (int) config('races.bonus_amount');
 
-        $registrationPrice = $championship->registration_price ?? (int) config('races.price'); // TODO: read from config if not set
+        $registrationPrice = $championship->registration_price ?? (int) config('races.price');
 
         $bonus = $championship->bonuses()->licenceHash($event->participant->driver_licence)->first();
+
+        // Check if bonus already applied to participant
+        if ($event->participant->bonuses()->exists()) {
+            return;
+        }
 
         if (blank($bonus)) {
             return;
@@ -41,7 +46,11 @@ class ApplyBonusToParticipant
 
         $event->participant->update(['use_bonus' => true]);
 
-        // $event->participant->bonuses()->attach($bonus);
+        if ((bool) config('races.bonus_use_one_at_time')) {
+            $event->participant->bonuses()->attach($bonus);
+
+            return;
+        }
 
         // Calculate how many bonuses can be applied based on the registration price
         $bonusCount = intdiv($registrationPrice, $fixedBonusAmount);

5e03504cfc49405667a4b1d54f7f94c88a3c3537
Alessio Vertemati <alessio.vertemati@gmail.com>
2025-08-11 13:45:29 +0200

    Upgrade Laravel

--- config/session.php
+++ config/session.php
@@ -130,7 +130,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'krm'), '_').'_session'
+        Str::slug(env('APP_NAME', 'krm')).'-session'
     ),
 
     /*

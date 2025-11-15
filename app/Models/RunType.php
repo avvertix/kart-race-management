f383320d7fc49b3a914c24a4ad761bb965d7c15e
Alessio <avvertix@users.noreply.github.com>
2025-11-15 20:50:23 +0100

    Session runs and their identification

--- app/Models/RunType.php
+++ app/Models/RunType.php
@@ -13,24 +13,43 @@ enum RunType: int
     case RACE_1 = 30;
     case RACE_2 = 40;
 
-    public static function fromString(string $value): static
+    public static function fromString(?string $value): static
     {
-        if(Str::contains($value, ['gara 1', 'race 1'], ignoreCase: true)){
+        if(blank($value)){
+            throw new InvalidArgumentException("Cannot identify run from an empty text.");
+        }
+
+        if(Str::contains($value, ['gara 1', 'race 1', 'gara1', 'race1', 'prefinale', 'pre-finale'], ignoreCase: true)){
             return static::RACE_1;
         }
-        
-        if(Str::contains($value, ['gara 2', 'race 2'], ignoreCase: true)){
+
+        if(Str::contains($value, ['gara 2', 'race 2', 'gara2', 'race2', 'finale', 'final'], ignoreCase: true)){
             return static::RACE_2;
         }
-        
-        if(Str::contains($value, ['prove cronometrate', 'cronometrate'], ignoreCase: true)){
+
+        if(Str::contains($value, ['qualifiche', 'qualifying', 'prove cronometrate', 'cronometrate'], ignoreCase: true)){
             return static::QUALIFY;
         }
-        
-        if(Str::contains($value, ['prove libere', 'libere'], ignoreCase: true)){
+
+        if(Str::contains($value, ['prove libere', 'libere', 'warm up', 'warmup', 'warm-up', 'practice'], ignoreCase: true)){
             return static::WARM_UP;
         }
-        
-        throw new InvalidArgumentException("Cannot identify the run from the [{$value}]");
+
+        throw new InvalidArgumentException("Cannot identify run from [{$value}].");
+    }
+
+    public function isPractice(): bool
+    {
+        return $this === static::WARM_UP;
+    }
+
+    public function isQualify(): bool
+    {
+        return $this === static::QUALIFY;
+    }
+
+    public function isRace(): bool
+    {
+        return in_array($this, [static::RACE_1, static::RACE_2]);
     }
 }

43a6a46e6b485569e0d63f4d7563145146a81d0f
Alessio Vertemati <alessio.vertemati@gmail.com>
2025-12-31 12:16:21 +0100

    Update existing race types according to the new classification

---
+++ operations/2025_12_31_105056_compact_race_types.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Race;
+use App\Models\RaceType;
+use TimoKoerber\LaravelOneTimeOperations\OneTimeOperation;
+
+return new class extends OneTimeOperation
+{
+    /**
+     * Determine if the operation is being processed asynchronously.
+     */
+    protected bool $async = false;
+
+    /**
+     * The queue that the job will be dispatched to.
+     */
+    protected string $queue = 'default';
+
+    /**
+     * A tag name, that this operation can be filtered by.
+     */
+    protected ?string $tag = null;
+
+    /**
+     * Process the operation.
+     */
+    public function process(): void
+    {
+        // Migrate REGIONAL (20) and ZONE (30) race types to NATIONAL (40)
+        Race::query()
+            ->whereIn('type', [20, 30]) // REGIONAL = 20, ZONE = 30
+            ->update(['type' => RaceType::NATIONAL]);
+    }
+};

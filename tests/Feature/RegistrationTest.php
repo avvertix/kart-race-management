8f0b7fc6643d119e19d4b10b3c4f952009a7c4e8
Alessio <avvertix@users.noreply.github.com>
2026-01-25 18:37:57 +0100

    Enable public account creation

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
-use Laravel\Fortify\Features;
 use Laravel\Jetstream\Jetstream;
 use Plannr\Laravel\FastRefreshDatabase\Traits\FastRefreshDatabase;
 use Tests\TestCase;
@@ -16,32 +16,13 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered()
     {
-        if (! Features::enabled(Features::registration())) {
-            return $this->markTestSkipped('Registration support is not enabled.');
-        }
-
         $response = $this->get('/register');
 
         $response->assertStatus(200);
     }
 
-    public function test_registration_screen_cannot_be_rendered_if_support_is_disabled()
-    {
-        if (Features::enabled(Features::registration())) {
-            return $this->markTestSkipped('Registration support is enabled.');
-        }
-
-        $response = $this->get('/register');
-
-        $response->assertStatus(404);
-    }
-
     public function test_new_users_can_register()
     {
-        if (! Features::enabled(Features::registration())) {
-            return $this->markTestSkipped('Registration support is not enabled.');
-        }
-
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
@@ -52,5 +33,31 @@ class RegistrationTest extends TestCase
 
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
+
+        $user = User::query()->where('email', 'test@example.com')->firstOrFail();
+
+        $this->assertEquals('driver', $user->role);
+    }
+
+    public function test_honeypot_blocks_bot_registration()
+    {
+        $response = $this
+            ->from('/register')
+            ->post('/register', [
+                'name' => 'Bot User',
+                'email' => 'bot@example.com',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+                'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+                'website' => 'https://spam-site.com',
+            ]);
+
+        $this->assertGuest();
+        $response->assertRedirect('/register');
+        $response->assertSessionHasErrors('website');
+
+        $this->assertDatabaseMissing('users', [
+            'email' => 'bot@example.com',
+        ]);
     }
 }

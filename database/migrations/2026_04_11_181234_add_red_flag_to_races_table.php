5b8b0828b9060f4cad28b39b7ce784f3c88e61ef
avvertix <5672748+avvertix@users.noreply.github.com>
2026-04-11 18:51:24 +0000

    Fix styling

--- database/migrations/2026_04_11_181234_add_red_flag_to_races_table.php
+++ database/migrations/2026_04_11_181234_add_red_flag_to_races_table.php
@@ -23,8 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('races', function (Blueprint $table) {
-            
-        });
+        Schema::table('races', function (Blueprint $table) {});
     }
 };

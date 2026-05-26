2616211451ce9a061b83e3953001cfe5886bf7e9
Alessio Vertemati <alessio.vertemati@gmail.com>
2026-05-26 20:40:51 +0200

    Remove ownership of participation when account deleted

--- tests/Feature/DeleteAccountTest.php
+++ tests/Feature/DeleteAccountTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use App\Models\Participant;
 use App\Models\User;
 use Laravel\Jetstream\Features;
 use Laravel\Jetstream\Http\Livewire\DeleteUserForm;
@@ -44,5 +45,25 @@ class DeleteAccountTest extends TestCase
             ->assertHasErrors(['password']);
 
         $this->assertNotNull($user->fresh());
+    }
+
+    public function test_participant_links_are_cleared_when_user_is_deleted()
+    {
+        if (! Features::hasAccountDeletionFeatures()) {
+            return $this->markTestSkipped('Account deletion is not enabled.');
+        }
+
+        $this->actingAs($user = User::factory()->create());
+
+        $addedByParticipant = Participant::factory()->create(['added_by' => $user->id]);
+        $claimedByParticipant = Participant::factory()->create(['claimed_by' => $user->id]);
+
+        Livewire::test(DeleteUserForm::class)
+            ->set('password', 'password')
+            ->call('deleteUser');
+
+        $this->assertNull($user->fresh());
+        $this->assertNull($addedByParticipant->fresh()->added_by);
+        $this->assertNull($claimedByParticipant->fresh()->claimed_by);
     }
 }
